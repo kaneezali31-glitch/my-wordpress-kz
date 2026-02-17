11f75688bb710c47bb47fb83b9591936838ee73a
kaneezali31-glitch <kaneezali31@gmail.com>
2026-02-17 23:56:02 +0500

    i add file named about.php

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+  $heading = "<h2 style='border-bottom: 2px solid pink; color: #e91e63;'>About Our Brand</h2>";
+  $mission = "<p style='background-color: #f9f9f9; padding: 10px;'>Our mission is to make <b>Fashion</b> affordable and <i>accessible</i> for everyone.</p>";
+  
+  echo $heading;
+  echo $mission;
+?>
