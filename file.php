544a752f16d158cd85e553e45a3c96dce4c54e52
kaneezali31-glitch <kaneezali31@gmail.com>
2026-02-18 15:41:04 +0500

    i added new file

---
+++ file.php
@@ -0,0 +1,7 @@
+<?php
+  $contactTitle = "<h3>Contact Info</h3>";
+  $email = "<p>Email: <b>kaneez@example.com</b></p>";
+  
+  echo $contactTitle;
+  echo $email;
+?>
---
+++ home.php
@@ -0,0 +1,7 @@
+<?php
+  $status = "<h1>Project Status</h1>";
+  $version = "<p>Current Version: <b>1.0</b> (<i>Stable</i>)</p>";
+  
+  echo $status;
+  echo $version;
+?>
