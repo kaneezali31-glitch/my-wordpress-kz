8f1a7e1b836af0b931b5f37cf0debbc01a5abeb7
kaneezali31-glitch <kaneezali31@gmail.com>
2026-02-18 16:57:03 +0500

    i made chnages

---
+++ monday.php
@@ -0,0 +1,31 @@
+<?php
+ $totalstudent="20";
+ $totalpresent="20";
+
+ $maths="sarey bache hai aj bum maths prhege";
+ $arts="sb nahi aw hai to arts prhege";
+if ($totalpresent==$totalstudent) {
+     echo "$maths";
+}
+else {
+    echo "$arts";
+    
+
+}
+    echo"<br>";
+$fruits=["apple", "banana"];
+print_r($fruits);
+
+
+echo"<br>";
+$automatic=array("washing machine"=>array("color"=>"white",
+"price"=>"100,000"));
+
+$machine=array($automatic);
+
+print_r($machine);
+  
+
+
+
+?>
